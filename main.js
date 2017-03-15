eb65636b394edd67e94c4235b8bb485966a7e2b9
sarah niemiec <sroseniemiec@gmail.com>
2017-03-15 16:53:34 -0400

    first commit of homework

---
+++ main.js
@@ -0,0 +1,7 @@
+var numberOfDonuts = 16;
+var glassesOfMilk = 12;
+console.log(numberOfDonuts + glassesOfMilk);
+
+var stringOne = "I do not like green eggs and ham. I do not like them, Sam-I-Am. ";
+var stringTwo = "Try them, try them, and you may! Try them and you may, I say.";
+console.log(stringOne + stringTwo);
